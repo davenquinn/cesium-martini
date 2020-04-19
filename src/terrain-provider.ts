59d8dec2de5bf68fc37b13e1e57fa4f963319977
Daven Quinn <dev@davenquinn.com>
2020-04-19 13:27:54 -0500

    basic attempts at terrain to grid

--- src/terrain-provider.ts
+++ src/terrain-provider.ts
@@ -5,25 +5,68 @@ import {
   Ellipsoid,
   WebMercatorTilingScheme
 } from "cesium"
+import Martini from '@mapbox/martini'
+
+function mapboxTerrainToGrid(png) {
+    const gridSize = png.width + 1;
+    const terrain = new Float32Array(gridSize * gridSize);
+
+    const tileSize = png.width;
+
+    // decode terrain values
+    for (let y = 0; y < tileSize; y++) {
+        for (let x = 0; x < tileSize; x++) {
+            const k = (y * tileSize + x) * 3;
+            const r = png.data[k + 0];
+            const g = png.data[k + 1];
+            const b = png.data[k + 2];
+            terrain[y * gridSize + x] = (r * 256 * 256 + g * 256.0 + b) / 10.0 - 10000.0;
+        }
+    }
+    // backfill right and bottom borders
+    for (let x = 0; x < gridSize - 1; x++) {
+        terrain[gridSize * (gridSize - 1) + x] = terrain[gridSize * (gridSize - 2) + x];
+    }
+    for (let y = 0; y < gridSize; y++) {
+        terrain[gridSize * y + gridSize - 1] = terrain[gridSize * y + gridSize - 2];
+    }
+
+    return terrain;
+}
 
 // https://github.com/CesiumGS/cesium/blob/1.68/Source/Scene/MapboxImageryProvider.js#L42
 
 class MapboxTerrainProvider extends CesiumTerrainProvider {
+  martini: any
   constructor(opts) {
     console.log(opts)
     super(opts)
+    this.martini = new Martini(257);
+
   }
 
-  requestMapboxTile (x, y, z) {
+  async requestMapboxTile (x, y, z) {
     const access_token = process.env.MAPBOX_API_TOKEN
     const mx = this.tilingScheme.getNumberOfYTilesAtLevel(z)
 
     // Something wonky about our tiling scheme, perhaps
+    // 12/2215/2293 @2x
     const url =  `https://api.mapbox.com/v4/mapbox.terrain-rgb/${z+1}/${x}/${y}.pngraw?access_token=${access_token}`
 
-    window.fetch(url).then(res => {
-      console.log(res)
-    })
+    const v = await window.fetch(url)
+    const data = await v.body.getReader().read()
+    const terrain = mapboxTerrainToGrid({data: data.value, width: 256, height: 256})
+    console.log(terrain)
+
+    // set up mesh generator for a certain 2^k+1 grid size
+    // generate RTIN hierarchy from terrain data (an array of size^2 length)
+    const tile = this.martini.createTile(terrain);
+    console.log(tile)
+
+    // get a mesh (vertices and triangles indices) for a 10m error
+    const mesh = tile.getMesh(4);
+    console.log(mesh)
+
 
   }
 
