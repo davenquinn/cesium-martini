6d515d9d6dddc19cb7b1421d8ba3389cd51664d3
Daven Quinn <dev@davenquinn.com>
2020-04-18 23:55:40 -0500

    basic cesium viewer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('Hello, world.')
+import "cesium/Widgets/widgets.css"
+import "./main.css"
+// @ts-ignore
+import * as Cesium from "cesium/Cesium"
+
+var opts = {
+  terrainProvider: Cesium.createWorldTerrain(),
+  imageryProvider : Cesium.createWorldImagery({
+      style : Cesium.IonWorldImageryStyle.AERIAL
+  }),
+  //baseLayerPicker : false,
+  geocoder: false,
+  //skyAtmosphere: true,
+  animation: false,
+  timeline: false,
+  // Makes cesium not render high fps all the time
+  requestRenderMode : true,
+  // Use full scene buffer (respecting pixel ratio) if this is false
+  useBrowserRecommendedResolution: false
+}
+
+
+Cesium.Ion.defaultAccessToken = process.env.CESIUM_ACCESS_TOKEN;
+
+const domID = "cesium-container"
+const g = document.createElement('div');
+g.id = domID;
+document.body.appendChild(g)
+
+var viewer = new Cesium.Viewer(domID, opts)
+//viewer.resolutionScale = 2
+//viewer.scene.globe.enableLighting = true
+//viewer.canvas.style.imageRendering = false
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {join} = require("path")
+const CopyPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const {DefinePlugin} = require("webpack")
+const path = require("path")
+
+const cesiumSource = 'node_modules/cesium/Source'
+const cesiumWorkers = '../Build/Cesium/Workers'
 
 module.exports = {
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      // CesiumJS module name
+      cesium: path.resolve(__dirname, cesiumSource)
+    }
   },
   module: {
+    unknownContextCritical: false,
     rules: [
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ]
+        use: ["ts-loader"]
+      },
+      {
+        test: /\.(png|svg)$/,
+        use: ['file-loader'],
       },
       {test: /\.css$/, use: ["style-loader", 'css-loader' ]},
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
@@ -27,11 +38,26 @@ module.exports = {
       }
     ]
   },
+  node: {
+    fs: 'empty'
+  },
   entry: "./src/index.ts",
   output: {
-    path: join(__dirname, "dist")
+    path: path.join(__dirname, "dist"),
+    sourcePrefix: ""
+  },
+  amd: {
+      // Enable webpack-friendly use of require in Cesium
+      toUrlUndefined: true
   },
   plugins: [
     new HtmlWebpackPlugin({title: "Mapbox / Cesium Terrain"}),
+    new CopyPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' } ]),
+    new CopyPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets' } ]),
+    new CopyPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' } ]),
+    new DefinePlugin({
+        // Define relative base path in cesium for loading assets
+        CESIUM_BASE_URL: JSON.stringify("/")
+    })
   ]
 };
