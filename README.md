c8db018a8d1c1e585227687b1660dc1d745057e9
Daven Quinn <dev@davenquinn.com>
2020-04-18 23:10:24 -0500

    Compiles with webpack

---
+++ README.md
@@ -0,0 +1,2 @@
+https://observablehq.com/@mourner/martin-real-time-rtin-terrain-mesh
+https://cesium.com/docs/cesiumjs-ref-doc/TerrainProvider.html
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {join} = require("path")
+
 module.exports = {
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
@@ -24,13 +27,11 @@ module.exports = {
       }
     ]
   },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    react: "React",
-    "react-dom": "ReactDOM"
-  }
+  entry: "./src/index.ts",
+  output: {
+    path: join(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({title: "Mapbox / Cesium Terrain"}),
+  ]
 };
