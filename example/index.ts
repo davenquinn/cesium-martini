3591ade42863b69108a4ba41da38fc1904951b8f
Daven Quinn <dev@davenquinn.com>
2021-05-16 01:47:23 -0500

    Some WIP updates

--- example/index.ts
+++ example/index.ts
@@ -1,24 +1,25 @@
-import "cesiumSource/Widgets/widgets.css"
-import "./main.css"
-const Cesium: any = require('cesiumSource/Cesium')
+import "cesiumSource/Widgets/widgets.css";
+import "./main.css";
+const Cesium: any = require("cesiumSource/Cesium");
 // Import @types/cesium to use along with CesiumJS
-import { Viewer, Ion, IonResource } from 'cesium';
-import TerrainProvider from "../src/terrain-provider"
+import { Viewer, Ion, IonResource } from "cesium";
+import TerrainProvider from "../dist";
+import regeneratorRuntime from "regenerator-runtime";
 
 const terrainProvider = new TerrainProvider({
-    // @ts-ignore
-    url: IonResource.fromAssetId("1"),
-    requestVertexNormals: false,
-    requestWaterMask: false,
-    accessToken: process.env.MAPBOX_API_TOKEN,
-    highResolution: true
+  // @ts-ignore
+  url: IonResource.fromAssetId("1"),
+  requestVertexNormals: false,
+  requestWaterMask: false,
+  accessToken: process.env.MAPBOX_API_TOKEN,
+  highResolution: true,
 });
 
 let satellite = new Cesium.MapboxImageryProvider({
-  mapId : 'mapbox.satellite',
-  maximumLevel : 19,
-  accessToken: process.env.MAPBOX_API_TOKEN
-})
+  mapId: "mapbox.satellite",
+  maximumLevel: 19,
+  accessToken: process.env.MAPBOX_API_TOKEN,
+});
 
 var opts = {
   terrainProvider,
@@ -27,7 +28,7 @@ var opts = {
   // }),
   // @ts-ignore
   skyBox: false as false,
-  baseLayerPicker : false,
+  baseLayerPicker: false,
   geocoder: false,
   skyAtmosphere: false as false,
   animation: false,
@@ -39,39 +40,42 @@ var opts = {
   // We have a bug in the tile bounding box calculation somewhere.
   terrainExaggeration: 1.000001,
   imageryProvider: satellite,
-}
+};
 
-
-const domID = "cesium-container"
-const g = document.createElement('div');
+const domID = "cesium-container";
+const g = document.createElement("div");
 g.id = domID;
-document.body.appendChild(g)
+document.body.appendChild(g);
 
-var clat = -21.133786
-var clon = 14.5481193
+var clat = -21.133786;
+var clon = 14.5481193;
 
-const rect = Cesium.Rectangle.fromDegrees(clon-0.01, clat-0.01, clon+0.01, clat+0.01)
+const rect = Cesium.Rectangle.fromDegrees(
+  clon - 0.01,
+  clat - 0.01,
+  clon + 0.01,
+  clat + 0.01
+);
 //Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rect;
 //Cesium.Camera.DEFAULT_VIEW_FACTOR = 0.005;
 //Cesium.Camera.DEFAULT_VIEW_OFFSET = new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 1)
 
-var viewer = new Cesium.Viewer(domID, opts)
+var viewer = new Cesium.Viewer(domID, opts);
 
 //viewer.scene.globe.baseColor = Cesium.Color.AQUAMARINE
 // @ts-ignore
 //viewer.scene.globe._surface._tileProvider._debug.wireframe = true
 // @ts-ignore
-viewer.extend(Cesium.viewerCesiumInspectorMixin)
+viewer.extend(Cesium.viewerCesiumInspectorMixin);
 
-
-var extent = Cesium.Cartesian3.fromDegrees(clon, clat-0.3, 8000)
+var extent = Cesium.Cartesian3.fromDegrees(clon, clat - 0.3, 8000);
 viewer.camera.setView({
-    destination : extent,
-    orientation: {
-        heading : Cesium.Math.toRadians(0), // east, default value is 0.0 (north)
-        pitch : Cesium.Math.toRadians(-15),    // default value (looking down)
-        roll : 0.0                             // default value
-    }
+  destination: extent,
+  orientation: {
+    heading: Cesium.Math.toRadians(0), // east, default value is 0.0 (north)
+    pitch: Cesium.Math.toRadians(-15), // default value (looking down)
+    roll: 0.0, // default value
+  },
 });
 
 //viewer.resolutionScale = 2
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const DotenvPlugin = require('dotenv-webpack')
-const {DefinePlugin} = require("webpack")
-const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const DotenvPlugin = require("dotenv-webpack");
+const { DefinePlugin } = require("webpack");
+const path = require("path");
 
-const cesiumSource = 'node_modules/cesium/Source'
-const cesiumWorkers = '../Build/Cesium/Workers'
+const cesiumSource = "node_modules/cesium/Source";
+const cesiumWorkers = "../Build/Cesium/Workers";
 
 module.exports = {
   // Enable sourcemaps for debugging webpack's output.
@@ -14,8 +14,8 @@ module.exports = {
     extensions: [".ts", ".tsx", ".js"],
     alias: {
       // CesiumJS module name
-      cesiumSource: path.resolve(__dirname, cesiumSource)
-    }
+      cesiumSource: path.resolve(__dirname, cesiumSource),
+    },
   },
   module: {
     unknownContextCritical: false,
@@ -23,42 +23,46 @@ module.exports = {
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: ["ts-loader"]
+        use: ["babel-loader"],
       },
       {
         test: /\.(png|svg)$/,
-        use: ['file-loader'],
+        use: ["file-loader"],
       },
-      {test: /\.css$/, use: ["style-loader", 'css-loader' ]},
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         enforce: "pre",
         test: /\.js$/,
-        loader: "source-map-loader"
-      }
-    ]
+        loader: "source-map-loader",
+      },
+    ],
   },
   node: {
-    fs: 'empty'
+    fs: "empty",
   },
   entry: "./example/index.ts",
   output: {
-    path: path.join(__dirname, "dist"),
-    sourcePrefix: ""
+    path: path.join(__dirname, "example-dist"),
+    sourcePrefix: "",
   },
   amd: {
-      // Enable webpack-friendly use of require in Cesium
-      toUrlUndefined: true
+    // Enable webpack-friendly use of require in Cesium
+    toUrlUndefined: true,
   },
   plugins: [
-    new HtmlWebpackPlugin({title: "Mapbox / Cesium Terrain"}),
-    new CopyPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' } ]),
-    new CopyPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets' } ]),
-    new CopyPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' } ]),
+    new HtmlWebpackPlugin({ title: "Mapbox / Cesium Terrain" }),
+    new CopyPlugin([
+      { from: path.join(cesiumSource, cesiumWorkers), to: "Workers" },
+    ]),
+    new CopyPlugin([{ from: path.join(cesiumSource, "Assets"), to: "Assets" }]),
+    new CopyPlugin([
+      { from: path.join(cesiumSource, "Widgets"), to: "Widgets" },
+    ]),
     new DotenvPlugin(),
     new DefinePlugin({
-        // Define relative base path in cesium for loading assets
-        CESIUM_BASE_URL: JSON.stringify("/")
-    })
-  ]
+      // Define relative base path in cesium for loading assets
+      CESIUM_BASE_URL: JSON.stringify("/"),
+    }),
+  ],
 };
